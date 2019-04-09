4d7741b2e4b055c8a27bc385ee842014c8335797
rubygeek <nola@rubygeek.com>
2019-04-08 20:14:44 -0500

    some queries we figured out during the meeting

--- src/clojure_meeting_asami/core.clj
+++ src/clojure_meeting_asami/core.clj
@@ -7,13 +7,19 @@
 (def store (r/get-storage-handle {:type :memory}))
 
 (let [id1 (store/new-node store)
-      id2 (store/new-node store)]
+      id2 (store/new-node store)
+      id3 (store/new-node store)
+      id4 (store/new-node store)]
 
   ;; No entities like DS  
   (def datoms [[id1 :name "Bob"]
                [id1 :age 30]
                [id2 :name "Sally"]
-               [id2 :age 15]]))
+               [id2 :age 15]
+               [id3 :name "George"]
+               [id3 :age 15]
+               [id4 :name "Sam"]
+               [id4 :age 15]]))
 
 (def db (store/assert-data store datoms))
 
@@ -24,4 +30,10 @@
                [?e :age ?a]
                [(< ?a ?min-age)]])
 
-(a/q q-young db 18)
+(def q-names '[:find ?name ?age
+               :where [?e :name ?name]
+                      [?e :age ?age]])
+
+(a/q q-young db 16)
+
+(a/q q-names db)
--- src/clojure_meeting_datascript/core.clj
+++ src/clojure_meeting_datascript/core.clj
@@ -5,28 +5,36 @@
 
 
 (def schema { :person/name {:db/cardinality :db.cardinality/one
-                            :db/valueType   :db.type/string
+                            #_:db/valueType   #_:db.type/string
                             :db/doc         "A person's name"}
              
               :person/age  {:db/cardinality :db.cardinality/one
-                            :db/valueType   :db.type/string
+                            #_:db/valueType   #_:db.type/string
                             :db/doc         "A person's age"}})
 
 ;; created with no schema
-;; (def conn (d/create-conn))
+(def conn (d/create-conn))
 
 
 (def conn (d/create-conn schema))
 
 ;; Define entities to transact
 (def entities [{:db/id -1 :name "Bob" :age 30}
-               {:db/id -2 :name "Sally" :age 15}])
+               {:db/id -2 :name "Sally" :age 15}
+               {:db/id -6 :name "Molly" :age 16}
+               {:db/id -7 :name "Brad" :age 16}
+               {:db/id -8 :name "Kirk" :age 16}
+               {:db/id -9 :name "Amanda" :age 16}
+               ])
 
 ;;; Add the entities via transaction
 (d/transact! conn entities)
 
+conn
+
+
 ;; datoms via transaction
-(d/transact! conn [{:db/id -3 :name "george"}])
+
 (d/transact! conn [{:db/id -4 :name "mary"}])
 (d/transact! conn [{:db/id -4 :age "13"}])
 
@@ -41,8 +49,27 @@ conn
                [?e :age ?a]
                [(< ?a ?min-age)]])
 
+(def q-names '[:find ?name ?age
+               :where [?e :name ?name]
+                      [?e :age  ?age]
+               ])
+
+
+(def q-sixteens '[:find ?name
+                  :where [?e :name ?name]
+                         [?e  :age 16]])
+
+(def q-sixteens-ids '[:find ?e
+                      :where [?e  :age 16]])
+
 ;; execute query: q-young, passing 18 as min-age
-(d/q q-young @conn 18)
+(d/q q-young @conn 13)
+
+(d/q q-names @conn)
+(d/q q-sixteens @conn)
+(d/q q-sixteens-ids @conn)q
+@conn
+
 
 
 
